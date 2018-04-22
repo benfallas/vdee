fe9838501d4bb97f93d2cba5b010afe26877b867
madr8615 <fmadrigal@csumb.edu>
2018-04-21 18:19:50 -0700

    Adding list of Books to Biblia Screen
    Summary: Adding List of Books to Biblia Screen
    
    Test Plan:
    Screen shot.
    Verify that it works given good network.
    
    Reviewers: benFallas
    
    Reviewed By: benFallas
    
    Differential Revision: https://vddeterno.phacility.com/D42

--- app/src/main/java/vdee/vdee/bibleScreen/BibleLayout.java
+++ app/src/main/java/vdee/vdee/bibleScreen/BibleLayout.java
@@ -1,10 +1,17 @@
 package vdee.vdee.bibleScreen;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import rx.Subscriber;
 import vdee.vdee.R;
+import vdee.vdee.data.module.booksResponse.Book;
 import vdee.vdee.data.module.booksResponse.Books;
 import vdee.vdee.data.module.booksResponse.BooksResponse;
 import vdee.vdee.views.layout.toolbar.ToolbarSupport;
@@ -13,10 +20,15 @@ import vdee.vdee.views.layout.toolbar.backArrowToolbar.BackArrowToolbar;
 /**
  *Bible Layout holds the logic for most activity happening on Main Screen.
  */
-public class BibleLayout extends Subscriber<BooksResponse>{
+public class BibleLayout extends Subscriber<BooksResponse> implements BooksAdapter.ViewHolderListener {
 
     private BibleActivity mBibleActivity;
     private ToolbarSupport mToolbar;
+    private BooksAdapter mAdapater;
+
+    @BindView(R.id.recycler_view_books) RecyclerView mRecyclerViewBooks;
+
+    private ArrayList<Book> books;
 
     public BibleLayout(BibleActivity bibleActivity) {
         mBibleActivity = bibleActivity;
@@ -30,13 +42,24 @@ public class BibleLayout extends Subscriber<BooksResponse>{
     public void onCompleted() { }
 
     @Override
-    public void onError(Throwable e) {
-        Log.d("bibleResponse", e.getMessage());
-    }
+    public void onError(Throwable e) { }
 
     @Override
     public void onNext(BooksResponse response) {
-        Log.d("bibleResponse", response.toString());
+        displayBooks(response.getResponse().getBooks());
+    }
 
+    private void displayBooks(ArrayList<Book> books) {
+        mAdapater = new BooksAdapter(mBibleActivity.getApplicationContext(),
+                books,
+                this);
+
+        mRecyclerViewBooks.setLayoutManager(new LinearLayoutManager(mBibleActivity.getApplicationContext()));
+        mRecyclerViewBooks.setAdapter(mAdapater);
+    }
+
+    @Override
+    public void onBibleBookClicked(int position) {
+        Toast.makeText(mBibleActivity.getApplicationContext(), "TESt", Toast.LENGTH_SHORT).show();
     }
 }
